20c0d0a28f5a11631b9f7c51346defd3cc2677e1
hata <wmf8341@gmail.com>
2022-05-11 13:17:08 +0900

    #1 analytics追加　ios-v 10.0

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
