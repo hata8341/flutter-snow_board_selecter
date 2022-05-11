bbe17c5f3a7e27f5600b93cb9917534f314ff2cd
hata <wmf8341@gmail.com>
2022-05-11 12:31:04 +0900

    #1 firebase初期設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
