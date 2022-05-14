95451bceec0c2882f2e4478983e5cba2509bd72e
hata8341 <83937097+hata8341@users.noreply.github.com>
2022-05-14 22:28:56 +0900

    #25 firebase detail config (#26)
    * #25 analyticsのクラス作成
    
    * #25 crashlyticsの設定
    
    * #25 テストcrashイベントを作成
    
    * #25 create-crash-wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,91 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sbselector/firebase/firebase_analytics_config.dart';
+import 'package:sbselector/firebase/firebase_crashlytics_config.dart';
+import 'package:sbselector/firebase/firebase_options.dart';
 
-import 'firebase_options.dart';
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+// await Firebase.initializeApp(
+//   options: DefaultFirebaseOptions.currentPlatform,
+// );
+//   runApp(const MyApp());
+// }
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+void main() {
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    await CrashReporter.instance.initialize();
+
+    FlutterError.onError = (FlutterErrorDetails details) {
+      CrashReporter.instance.report(details.exceptionAsString(), details.stack);
+    };
+
+    runApp(const ProviderScope(child: MyApp('user_identify')));
+  }, (error, stack) {
+    CrashReporter.instance.report(error, stack);
+  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
-  static FirebaseAnalyticsObserver observer =
-      FirebaseAnalyticsObserver(analytics: analytics);
+// class MyApp extends StatelessWidget {
+//   const MyApp(this.id,{Key? key}) : super(key: key);
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+// debugShowCheckedModeBanner: false,
+// title: 'Flutter Demo',
+// theme: ThemeData(
+//   primarySwatch: Colors.blue,
+// ),
+// navigatorObservers: <NavigatorObserver>[AnalyticsServeice.observer],
+// home: const MyHomePage(title: 'Flutter Demo test3 Home Page'),
+//     );
+//   }
+// }
+
+class MyApp extends HookConsumerWidget {
+  const MyApp(this.id, {Key? key}) : super(key: key);
+
+  final String id;
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      ref.read(crashReporter).setIdentify(id);
+      return null;
+    }, []);
+
     return MaterialApp(
+      builder: (BuildContext context, widget) {
+        Widget error = const Text('表示エラー');
+        if (widget is Scaffold || widget is Navigator) {
+          error = Scaffold(
+              body: Center(
+            child: error,
+          ));
+        }
+        ErrorWidget.builder = (FlutterErrorDetails details) {
+          ref
+              .read(crashReporter)
+              .report(details.exceptionAsString(), details.stack);
+          return error;
+        };
+        return widget!;
+      },
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      navigatorObservers: <NavigatorObserver>[observer],
+      navigatorObservers: <NavigatorObserver>[AnalyticsServeice.observer],
       home: const MyHomePage(title: 'Flutter Demo test3 Home Page'),
     );
   }
@@ -48,6 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _counter++;
     });
+    FirebaseCrashlytics.instance.crash();
   }
 
   @override
