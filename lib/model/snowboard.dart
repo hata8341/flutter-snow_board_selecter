d350353c385911925d11c14359ed5788c18ba4a3
hata <wmf8341@gmail.com>
2022-05-28 15:51:32 +0900

    #37 create_snowboard_class

---
+++ lib/model/snowboard.dart
@@ -0,0 +1,19 @@
+// 一度代入できれば良いので、定数のクラスして使う（final class)
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'snowboard.freezed.dart';
+part 'snowboard.g.dart';
+
+@freezed
+class Snowboard with _$Snowboard {
+  const factory Snowboard({
+    required String name,
+    required String descprition,
+    // required String imageUrl,
+    // required List<double> chartData,
+  }) = _Snowboard;
+
+  factory Snowboard.fromJson(Map<String, dynamic> json) =>
+      _$SnowboardFromJson(json);
+}
---
+++ lib/model/snowboard.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'snowboard.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Snowboard _$$_SnowboardFromJson(Map<String, dynamic> json) => _$_Snowboard(
+      name: json['name'] as String,
+      descprition: json['descprition'] as String,
+    );
+
+Map<String, dynamic> _$$_SnowboardToJson(_$_Snowboard instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'descprition': instance.descprition,
+    };
